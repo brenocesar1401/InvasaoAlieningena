3336dfd6ff54a818d6196e54ff7d829a4f4bd25a
Breno <brenocesar1401@poli.ufrj.br>
2020-07-26 04:48:51 -0300

    Criação da tela inicial do jogo, com cor de fundo e nave.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,44 @@
 """
 Aqui será rodado o jogo
 """
+
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+ai_settings = Settings()
+
+
+def run_game():
+    #  Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
+
+    #  Inicia o laço principal do jogo
+    while True:
+
+        #  Observa eventos de teclado e de mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #  Redesenha a tela a cada passagem pelo laço
+        screen.fill(ai_settings.bg_color)
+
+        ship.blitme()
+
+        #  Deixa a tela mais recente visível
+        pygame.display.flip()
+
+
+run_game()
+
+
+
