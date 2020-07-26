b550d84d5fe5c32eb514abe086e52d38fe9007e4
Breno <brenocesar1401@poli.ufrj.br>
2020-07-26 04:49:46 -0300

    Criação de arquivo para classe da nave

---
+++ ship.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, screen):
+        """ Inicializa a nave e define sua posição inicial"""
+        self.screen = screen
+
+        #  Carrega a imagem da espaçonave e obtém seu rect
+        self.image = pygame.image.load('/home/jcoimbra/PycharmProjects/InvasaoAlieningena/images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #  Inicia cada nova espaçonave na parte inferior central da tela
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """ Desemja a nave em sua posição atual"""
+        self.screen.blit(self.image, self.rect)
+
